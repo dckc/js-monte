c70b9d91fd8e58597d56625af66b6648f9a51829
Dan Connolly <http://www.madmode.com/>
2015-08-07 23:50:56 -0500

    toward AST for kernel monte, with a start at loading

---
+++ kernel.js
@@ -0,0 +1,39 @@
+/* @flow */
+
+type Expression =
+  // Literals
+  { is: "null" } |
+  { is: "true" } |
+  { is: "false" } |
+  { is: "str", val: string } |
+  { is: "double", val: number } |
+  { is: "char", val: string } |  // no char type in JS :-/
+  { is: "int", val: number }  | // TODO: arbitrary precision int
+
+  { is: "noun", has: string };
+  // TODO: others
+
+function load(data: string): Expression {
+  return { is: "null" };
+}
+
+function makeStream(items: string) {
+    var counter = 0;
+    var size = items.length;
+
+    var unshift = (byte) => (byte - 32) % 256;
+    var nextItem = () => {
+	if (counter > size) {
+	    throw "Buffer underrun while streaming";
+	}
+	var rv = items.charCodeAt(counter);
+	counter += 1;
+	return rv;
+    };
+
+    return Object.freeze({
+	done: () => counter >= size,
+	nextItem: nextItem,
+	nextByte: () => unshift(nextItem())
+    });
+}
