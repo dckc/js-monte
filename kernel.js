5e816015e1ee44de2af4761d89c459a962f7f9c6
Dan Connolly <http://www.madmode.com/>
2015-08-08 23:15:16 -0500

    loadTerm() groks object etc.; can read nullPump.ty
      - add escape, finally, object to Expression type
        - add types for Script, Method, Matcher
      - refactor loadOpt() to work for Pattern etc. as well as Expression
      - testing and diagnostic refinements
        - make index.html into interactive test harness
        - factor out getTag() with optional expected tag
        - add mappings from AST format tags to names and back
      - include binding in Pattern type
      - un-export var trueExpr, falseExpr (no global mutable state!)
      - handle 0-length string

--- kernel.js
+++ kernel.js
@@ -12,16 +12,32 @@ export type Expression =
     { is: "seq", items: Array<Expression> } |
     { is: "noun", name: string } |
     { is: "def", pat: Pattern, guard: ?Expression, expr: Expression } |
-    { is: "call", target: Expression, verb: string, args: Array<Expression> }
+    { is: "call", target: Expression, verb: string, args: Array<Expression> } |
+    { is: "escape", pattern: Pattern, body: Expression,
+      catchPattern: ?Pattern, catchBody: Expression } |
+    { is: "finally", body: Expression, finalBody: Expression } |
+    { is: "object", doc: ?string, name: ?string,
+      as: ?Expression, impl: Array<Expression>, script: Script }
 ;
   // TODO: others
+
+type Script = { ext: ?Expression,
+		methods: Array<Method>,
+		matchers: Array<Matcher> };
+type Method = { doc: ?string,
+		verb: string,
+		params: Array<Pattern>,
+		guard: ?Expression,
+		body: Expression };
+type Matcher = { pattern: Pattern, body: Expression };
 
 export type Pattern =
     { pt: "final", name: string, guard: ?Expression } |
     { pt: "ignore", guard: ?Expression } |
     { pt: "var", name: string, guard: ?Expression } |
     { pt: "list", items: Array<Pattern>, tail: ?Pattern } |
-    { pt: "via", view: Expression, inner: Pattern }
+    { pt: "via", view: Expression, inner: Pattern } |
+    { pt: "binding", noun: string }
 ;
 
 function fix<T>(x: T): T {
@@ -38,8 +54,8 @@ function trace<T>(label, x: T): T {
 }
 
 export var nullExpr : Expression = fix({ is: "null" });
-export var trueExpr : Expression = fix({ is: "true" });
-export var falseExpr : Expression = fix({ is: "false" });
+var trueExpr : Expression = fix({ is: "true" });
+var falseExpr : Expression = fix({ is: "false" });
 
 export function boolExpr(b: bool) : Expression {
     return b ? trueExpr : falseExpr;
@@ -62,6 +78,58 @@ export function load(data: string): Array<Expression> {
 	terms.push(term);
     }
     return terms;
+}
+
+var tagName = [
+    'NULL',
+    'TRUE', 'FALSE',
+    'STR', 'FLOAT', 'CHAR', 'INT',
+    'TUPLE', 'BAG', 'ATTR',
+
+    'LiteralExpr',
+    'NounExpr',
+    'BindingExpr',
+    'SeqExpr',
+    'MethodCallExpr',
+    'Def',
+    'Escape',
+    'Object',
+    'Script',
+    'Method',
+    'Matcher',
+    'Assign',
+    'Finally',
+    'KernelTry',
+    'HideExpr',
+    'If',
+    'Meta',
+
+    'Final',
+    'Ignore',
+    'Var',
+    'List',
+    'Via',
+    'Binding',
+
+    'Character'
+];
+var tagNum = (function() {
+    var ret = {};
+    for (var i=0; i < tagName.length; i++) {
+	ret[tagName[i]] = i;
+    }
+    return ret;
+})();
+
+function getTag(stream, expected: ?string): number {
+    var t = stream.nextByte();
+    if (expected) {
+	var e = tagNum[expected];
+	if (e !== t) {
+	    throw new Error({expected: [e, expected], actual: [t, tagName[t]]})
+	}
+    }
+    return t;
 }
 
 function makeStream(items: string) {
@@ -133,14 +201,20 @@ function decode_utf8(s) {
 
 function loadString(s) {
     var size = s.nextVarInt(); //toInt
-    return decode_utf8(s.slice(size));
+    return decode_utf8(size > 0 ? s.slice(size) : "");
 };
 
-function loadTuple<S, T>(s, f: ((s: S) => T)): Array<T> {
-    var kind = s.nextByte();
-    if (kind != 7) {
-	throw new Error("expected Tuple (7); got: " + kind);
+function loadNounName(s): string {
+    var e = loadTerm(s);
+    if (e.is !== 'noun') {
+	throw new Error({expected: 'noun', actual: e});
     }
+    return e.name;
+}
+
+function loadTuple<S, T>(s, f: ((s: S) => T)): Array<T> {
+    getTag(s, 'TUPLE');
+
     var arity = trace("tuple arity", s.nextVarInt());
     // TODO: MAX_ARITY
     // ew... why can't I get map or a comprehension to work?!
@@ -151,28 +225,70 @@ function loadTuple<S, T>(s, f: ((s: S) => T)): Array<T> {
     return rv;
 };
 
-function loadOpt(s): ?Expression {
-    var e = loadTerm(s);
-    return e.is === 'null' ? null : e;
-}
-
 function badFormat(info) {
     throw new Error("bad format " + JSON.stringify(info));
 };
 
-function loadTerm(stream): Expression {
-    var kind = stream.nextByte();
+function loadTerm(stream, peek: ?number): Expression {
+    if (!peek) {
+	peek = getTag(stream);
+    }
+    var kind = trace('loadTerm kind: ' + tagName[peek], peek);
 
     function notImpl(): Expression {
 	throw new Error("not impl " + kind);
     };
 
-    function loadStrVal(s): string {
-	var e = loadTerm(s);
+    function loadStrVal(s, peek: ?number): string {
+	var e = loadTerm(s, peek);
 	if (e.is !== 'str') {
-	    throw new Error('expected str; got: ' + JSON.stringify(e));
+	    throw new Error({expected: 'str', actual: JSON.stringify(e)});
 	}
 	return e.val;
+    }
+
+    function loadObject(s): Expression {
+	var doc = trace('object doc', loadOpt(s, loadString));
+	var namePat = loadPattern(s);
+	var name = (namePat.pt === 'final' ? namePat.name :
+		    namePat.pt === 'ignore' ? null :
+		    badFormat({expected: "final or ignore pattern",
+			       actual: namePat}));
+	var auditors = loadTuple(s, loadTerm);
+	var as = auditors[0].is === 'null' ? null : auditors[0];
+	var impl = auditors.slice(1);
+
+	getTag(s, 'Script');
+
+	return fix({is: 'object',
+		    doc: doc,
+		    name: name,
+		    as: as,
+		    impl: impl,
+		    script: { ext: loadOpt(s, loadTerm),
+			      methods: loadTuple(s, loadMethod),
+			      matchers: loadTuple(s, loadMatcher) }});
+    }
+
+    function loadMethod(s): Method {
+	getTag(s, 'Method');
+
+	var docExpr = loadOpt(s, loadTerm);
+
+	return {
+	    doc: (!docExpr ? null :
+		  docExpr.is === 'str' ? docExpr.val :
+		  badFormat({expected: 'Doc string', actual: docExpr})),
+	    verb: loadStrVal(s),
+	    params: loadTuple(s, loadPattern),
+	    guard: loadOpt(s, loadTerm),
+	    body: loadTerm(s)
+	};
+    }
+
+    function loadMatcher(s): Matcher {
+	return { pattern: loadPattern(s),
+		 body: loadTerm(s) };
     }
 
     var kinds : Array<((s: any) => Expression)>= [
@@ -183,9 +299,9 @@ function loadTerm(stream): Expression {
 	(s) => notImpl(), // double
 	(s) => notImpl(), // char
 	(s) => intExpr(zzd(s.nextVarInt())),
-	(s) => badFormat({ exprKind: kind}), // we handle tuple elsewhere
-	(s) => badFormat({ exprKind: kind}), // bag
-	(s) => badFormat({ exprKind: kind}), // attr
+	(s) => badFormat({ actual: tagName[kind]}), // we handle tuple elsewhere
+	(s) => badFormat({ actual: tagName[kind]}), // bag
+	(s) => badFormat({ actual: tagName[kind]}), // attr
 
 	// 10: LiteralExpr
 	(s) => loadTerm(s),
@@ -196,9 +312,31 @@ function loadTerm(stream): Expression {
 		    target: loadTerm(s),
 		    verb: loadStrVal(s),
 		    args: loadTuple(s, loadTerm)}),
-	// 16: Def
+
+	// 15: Def
 	(s) => fix({is: 'def', pat: loadPattern(s),
-		    guard: loadOpt(s), expr: loadTerm(s)}),
+		    guard: loadOpt(s, loadTerm), expr: loadTerm(s)}),
+	(s) => fix({is: 'escape',
+		    pattern: loadPattern(s),
+		    body: loadTerm(s),
+		    catchPattern: loadOpt(s, loadPattern),
+		    catchBody: loadTerm(s)}),
+	(s) => loadObject(s),
+	(s) => badFormat({ actual: tagName[kind]}), // 18: 'Script',
+	(s) => badFormat({ actual: tagName[kind]}), // 19: 'Method',
+
+	(s) => badFormat({ actual: tagName[kind]}), // 20: 'Matcher',
+	(s) => notImpl(), // 21: 'Assign',
+	(s) => fix({ is: "finally", body: loadTerm(s),
+		     finalBody: loadTerm(s)}),  
+	(s) => notImpl(), // 23: 'KernelTry',
+	(s) => notImpl(), // 24: 'HideExpr',
+
+	(s) => notImpl(), // 25: 'If',
+	(s) => notImpl(), // 26: 'Meta',
+
+	// ...
+	// 33: 'Character',
     ];
 
     var loadKind = kinds[kind];
@@ -210,38 +348,31 @@ function loadTerm(stream): Expression {
 }
 
 
-function loadOptPattern(stream): ?Pattern {
-    var peek = stream.nextByte();
-    if (peek == 0) {
+function loadOpt<S, T>(stream, load: (s: S, peek: ?number) => T): ?T {
+    var peek = getTag(stream);
+    if (peek == tagNum['NULL']) {
 	return null;
     }
-    return loadPattern(stream, peek);
+    return load(stream, peek);
 }
 
 function loadPattern(stream, peek: ?number): Pattern {
-    var bias = 27;
     if (!peek) {
-	peek = stream.nextByte();
+	peek = getTag(stream);
     }
-    var kind = peek - bias;
-
-    function loadNounName(s): string {
-	var e = loadTerm(s);
-	if (e.is !== 'noun') {
-	    throw new Error('expected noun; got: ' + JSON.stringify(e));
-	}
-	return e.name;
-    }
+    var bias = tagNum['Final'];
+    var kind = trace('loadPattern kind: ' + tagName[peek], peek) - bias;
 
     var kinds: Array<((s: any) => Pattern)> = [
 	(s) => fix({pt: 'final', name: loadNounName(s),
-		    guard: loadOpt(s)}),
-	(s) => fix({pt: 'ignore', guard: loadOpt(s)}),
+		    guard: loadOpt(s, loadTerm)}),
+	(s) => fix({pt: 'ignore', guard: loadOpt(s, loadTerm)}),
 	(s) => fix({pt: 'var', name: loadNounName(s),
-		    guard: loadOpt(s)}),
+		    guard: loadOpt(s, loadTerm)}),
 	(s) => fix({pt: 'list', items: loadTuple(s, loadPattern),
-		    tail: loadOptPattern(s)}),
-	(s) => fix({pt: "via", view: loadTerm(s), inner: loadPattern(s) })
+		    tail: loadOpt(s, loadPattern)}),
+	(s) => fix({pt: "via", view: loadTerm(s), inner: loadPattern(s) }),
+	(s) => fix({pt: "binding", noun: loadNounName(s) })
     ];
 
     var loadKind = kinds[kind];
